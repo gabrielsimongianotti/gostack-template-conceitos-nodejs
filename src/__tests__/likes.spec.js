c2899090e208ac4d3ff42bd82ac2b34e65607549
unknown <gabriel.gianotti@orgsystem.com.br>
2020-06-18 21:56:37 -0300

    new tests to put and delete respositories/${id}/like

--- src/__tests__/likes.spec.js
+++ src/__tests__/likes.spec.js
@@ -26,11 +26,34 @@ describe("Likes", () => {
     expect(response.body).toMatchObject({
       likes: 2
     });
+
+    let responseEdit = await request(app).put(
+      `/repositories/${repository.body.id}/like`
+    ).send({
+      likes:7
+    });
+
+    expect(responseEdit.body).toMatchObject({
+      likes: 7
+    });
+    let responseDelete = await request(app).delete(
+      `/repositories/${repository.body.id}/like`
+    );
+
+    expect(responseDelete.body).toMatchObject({
+      likes: 0
+    });
   });
 
   it("should not be able to like a repository that does not exist", async () => {
     await request(app)
       .post(`/repositories/123/like`)
+      .expect(400);
+      await request(app)
+      .put(`/repositories/123/like`)
+      .expect(400);
+      await request(app)
+      .delete(`/repositories/123/like`)
       .expect(400);
   });
 });
